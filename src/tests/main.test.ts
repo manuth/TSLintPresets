b11b6512a00b4d866a9057e4121f3cdfd2384619
Manuel Thalmann <m@nuth.ch>
2019-06-07 00:56:23 +0200

    Add a test for the default config-file

--- src/tests/main.test.ts
+++ src/tests/main.test.ts
@@ -1,10 +1,31 @@
+import Assert = require("assert");
+import FileSystem = require("fs-extra");
+import Path = require("path");
+import { Configuration } from "tslint";
+
+/**
+ * Tests the integrity of a `tslint` configuration-file.
+ *
+ * @param path
+ * The path to the `tslint`-configuration to test.
+ */
+function testConfig(path: string)
+{
+    let dirName: string = Path.dirname(Path.resolve(path));
+    let rawConfig = require(path);
+    let config = Configuration.parseConfigFile(rawConfig, dirName, (fileName) => require(Path.resolve(fileName)));
+    Assert.strictEqual(config.rules.size > 0, true);
+}
+
 suite(
     "TSLintRulesets",
     () =>
     {
+
         test(
-            "Example...",
+            "Checking the integrity of the main config-file…",
             () =>
             {
+                testConfig("../..");
             });
     });
